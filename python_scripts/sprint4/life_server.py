ab8851cacfb6d442c402dcf442d8d5986895d443
EndePointe <endepointe@gmail.com>
2021-02-24 20:54:02 +0000

    transmit message

--- python_scripts/sprint4/life_server.py
+++ python_scripts/sprint4/life_server.py
@@ -5,12 +5,17 @@ PORT = 5000
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.bind((HOST, PORT))
-  s.listen()
+  s.listen(5)
   conn, addr = s.accept()
   with conn: 
     print('I am the life server, connected by', addr)
+    client_msg = ''
     while 1:
       data = conn.recv(1024)
+      client_msg += data.decode()
+      print(client_msg)
       if not data:
         break
       conn.sendall(data)
+    conn.close()
+    print('population client disconnected')
